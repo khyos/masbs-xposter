23ae7ce81a03b02c3205eacd8797cbcd42f0fc8e
khyos <58590160+khyos@users.noreply.github.com>
2023-10-13 20:11:16 +0200

    Fix NPM Package

--- postinstall.js
+++ postinstall.js
@@ -4,8 +4,9 @@ const fs = require('fs')
 const pathToCid = 'node_modules/multiformats/types/src/cid.d.ts';
 fs.readFile(pathToCid, 'utf8', function (readError, fileContent) {
     if (readError) {
-        console.err('postinstall.js error:');
-        return console.err(readError);
+        console.log('postinstall.js error:');
+        console.log(readError);
+        return;
     }
     let result = fileContent.replace(/export type MultibaseDecoder<Prefix>/g, 'export type MultibaseDecoder<Prefix extends string>');
     result = result.replace(/export type MultibaseEncoder<Prefix>/g, 'export type MultibaseEncoder<Prefix extends string>');
